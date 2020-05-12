9b99780584983e9a55bd580fce6c590f72309d28
NOURDINE BENLKAMLE <nouredine.benlkamle@devinweb.com>
2020-05-12 23:20:45 +0000

    finish promodoro first version with lifecycles

--- Promodoro/App.js
+++ Promodoro/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 
 
@@ -15,7 +15,7 @@ class CounterEven extends React.Component{
     }
 }
 
-export default class App extends React.Component {
+class Counter extends React.Component {
 
   constructor(props){
     super(props)
@@ -25,19 +25,53 @@ export default class App extends React.Component {
   }
 
   inc = () => {
+    console.log('d')
     this.setState(prevState => ({
       count: prevState.count+1
     }))
   }
 
   componentDidMount(){
-    setInterval(this.inc, 1000)
+    this.intervale = setInterval(this.inc, 1000)
   }
 
+  componentWillUnmount(){
+    clearInterval(this.intervale)
+  }
   render(){
     return (
       <View style={styles.appContainer}>
-        <CounterEven count={this.state.count} />
+        <Text style={styles.count} >{this.state.count}</Text>
+      </View>
+  
+    )
+  }
+}
+
+export default class App extends React.Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      showCounter: true
+    }
+  }
+
+  toggle = () => {
+    this.setState(prevState => ({
+      showCounter: !prevState.showCounter
+    }))
+  }
+
+  render() {
+    if(this.state.showCounter) return (
+      <View style={styles.appContainer}>
+        <Button onPress={() => this.toggle()} style={styles.padding} title='show or dismiss counter' />
+        <Counter />
+      </View>
+    )
+    else return (
+      <View style={styles.appContainer}>
+        <Button onPress={() => this.toggle()} style={styles.padding} title='show or dismiss counter'/>
       </View>
     )
     }
@@ -51,5 +85,8 @@ const styles = StyleSheet.create({
   },
   count: {
     fontSize: 48
+  },
+  padding: {
+    paddingTop: 50
   }
 });
