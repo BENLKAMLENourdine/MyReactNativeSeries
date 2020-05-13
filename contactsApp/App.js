1fd174fd7e5de0939a262cd2aab812a9d6f69b75
NOURDINE BENLKAMLE <nouredine.benlkamle@devinweb.com>
2020-05-13 00:18:19 +0000

    contacts with FlatList for optimization purpose

--- contactsApp/App.js
+++ contactsApp/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import { Button, ScrollView, StyleSheet, Text, View } from 'react-native';
+import { Button, FlatList, StyleSheet, Text, View } from 'react-native';
 import Constants from 'expo-constants';
 
-import contacts from './contacts'
+import contacts, { compareNames } from './contacts'
 
 const Contact = props => (
   <View style={styles.contactPadding}>
@@ -14,22 +14,30 @@ const Contact = props => (
 export default class App extends React.Component {
   state = {
     showContacts: false,
+    contacts: contacts
+  }
+
+  sort = () => {
+    this.setState(prevState => ({
+      contacts: [...prevState.contacts].sort(compareNames)
+    }))
   }
 
   toggleContacts = () => {
     this.setState(prevState => ({showContacts: !prevState.showContacts}))
   }
 
+  renderItem = obj => <Contact {...obj.item} />
   render() {
     return (
       <View style={styles.viewPadding}>
-        <Button title="toggle contacts" onPress={this.toggleContacts} />
+        <Button style={styles.buttonMargin} title="toggle contacts" onPress={this.toggleContacts} />
+        <Button title="sort contacts" onPress={this.sort} />
           {
-            this.state.showContacts && (<ScrollView>
-              {contacts.map(contact => (
-                <Contact {...contact} />
-              ))}
-            </ScrollView>)
+            this.state.showContacts && (<FlatList
+              renderItem={this.renderItem}
+              data={this.state.contacts}
+            />)
           }
       </View>
     );
@@ -39,6 +47,9 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   viewPadding: {
     paddingTop: Constants.statusBarHeight
+  },
+  buttonMargin: {
+    marginBottom: 20,
   },
   contactPadding: {
     padding: 20
